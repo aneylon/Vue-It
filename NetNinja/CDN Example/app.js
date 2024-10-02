709d7f50aa2ec513c01372a1ac4c42dee3e49ba3
aneylon <arlen.m.neylon@gmail.com>
2024-10-01 19:26:06 -0600

    Complete example.

--- NetNinja/CDN Example/app.js
+++ NetNinja/CDN Example/app.js
@@ -9,9 +9,21 @@ const app = Vue.createApp({
       title: "one",
       author: "person",
       age: 34,
+      x: 0,
+      y: 0,
+      books: [
+        { author: "one", title: "ONE", isFav: true },
+        { author: "two", title: "TWO", isFav: false },
+        { author: "three", title: "THREE", isFav: true },
+      ],
+      url: "http://www.thenetninja.co.uk",
     };
   },
   methods: {
+    toggleFav(book, id) {
+      // this.books[id].isFav = !this.books[id].isFav;
+      book.isFav = !book.isFav;
+    },
     changeTitle(title) {
       console.log("click it");
       this.title = "other stuff";
@@ -19,6 +31,19 @@ const app = Vue.createApp({
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(event, data) {
+      console.log(event.type);
+      if (data) console.log({ data });
+    },
+    handleMouseMove(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
