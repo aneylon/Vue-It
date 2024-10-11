59989321a3a3600c68ec8339f2bcc5bb84c4d509
aneylon <arlen.m.neylon@gmail.com>
2024-10-11 14:48:28 -0600

    Start Study Cards.

---
+++ Examples/StudyCards/StudyCards.js
@@ -0,0 +1,63 @@
+console.log("let's study it!");
+const app = Vue.createApp({
+  setup() {
+    console.log("yo?");
+  },
+  beforeMount() {
+    console.log("first things first");
+    this.shuffledCards = this.cards.slice();
+  },
+  data() {
+    return {
+      cards: [
+        {
+          question: "First",
+          answer: "First",
+          explain: "First",
+        },
+        {
+          question: "Second",
+          answer: "Second",
+          explain: "Second",
+        },
+        {
+          question: "Third",
+          answer: "Third",
+          explain: "Third",
+        },
+        {
+          question: "Fourth",
+          answer: "Fourth",
+          explain: "Fourth",
+        },
+      ],
+      shuffledCards: [],
+      currentCard: 0,
+      showAnswer: false,
+    };
+  },
+  methods: {
+    AddCard() {
+      console.log("Add card");
+    },
+    Know() {
+      console.log("Know it!");
+      this.Shuffle();
+    },
+    DontKnow() {
+      console.log("Don't Know");
+      this.Shuffle();
+    },
+    NotSure() {
+      console.log("Not Sure");
+      this.Shuffle();
+    },
+    Shuffle() {
+      console.log("Every day I'm shuffling");
+    },
+    toggleShowAnswer() {
+      this.showAnswer = !this.showAnswer;
+    },
+  },
+});
+app.mount("#app");
